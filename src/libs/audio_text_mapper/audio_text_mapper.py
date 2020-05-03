2b1448daa73c007ef4c81c521f0a243158a65c43
dword <camm2016@yandex.ru>
2020-05-03 22:23:58 +0300

    change manual levenshtein metrics methods to module Levenshtein

--- src/libs/audio_text_mapper/audio_text_mapper.py
+++ src/libs/audio_text_mapper/audio_text_mapper.py
@@ -3,6 +3,7 @@
 from collections import namedtuple
 import speech_recognition as sr
 from statistics import mean
+from Levenshtein import distance
 
 class ATMapper:
     def __init__(self, words, audio_paths, durs):
@@ -19,39 +20,20 @@ class ATMapper:
         audio = sr.AudioFile(audio_path)
         with audio as source:
             recorded = rec.record(source)
-            for time in range(0, dur, chunk_time)
+            for time in range(0, dur, chunk_time):
                 chunk_words = rec.recognize_google(recorded, language="ru-RU", duration=chunk_time).split(' ')
                 recognized_words.append(namedtuple('Chunk', ['text', 'tstart', 'tend'])
                 (chunk_words, time, time + min(dur - time, chunk_time)))
         return recognized_chunks
 
 
-    def calc_levenshtein(self, first, second, i, j, dist):
-        if i == 0 or j == 0:
-            dist[i][j] = max(i, j)
-        elif not(dist[i][j] is None):
-            return
-        else:
-            self.calc_levenshtein(first, second, i - 1, j, dist)
-            self.calc_levenshtein(first, second, i, j - 1, dist)
-            dist[i][j] = min(min(dist[i - 1][j], dist[i][j - 1]) + 1, dist[i - 1][j - 1] + first[i] != second[j])
-
-
-    def levenshtein_metrics(self, first, second):
-        first = ' ' + first
-        second = ' ' + second
-        dist = [[None for i in range(len(first))] for i in range(len(second))]
-        self.calc_levenshtein(first, second, 0, 0, dist)
-        return dist[-1][-1]
-
-
     def average_metrics(self, chunk, text_words):
-        return mean([self.levenshtein_metrics(chunk_word, text_word) for chunk_word, text_word in zip(chunk, text_words)
+        return mean([distance(chunk_word, text_word) for chunk_word, text_word in zip(chunk, text_words)])
 
 
-    def chunk_search(self, chunk, begin, end):
-        end = end - len(chunk) + 1
-        matches = [(i, self.average_metrics(chunk, words[i:i + len(chunk)])) for i in range(begin, end)]
+    def chunk_search(self, chunk, startp, endp):
+        endp = endp - len(chunk) + 1
+        matches = [(i, self.average_metrics(chunk, words[i:i + len(chunk)])) for i in range(startp, endp)]
         return min(poses, key=lambda match: match[1])[0]
 
 
@@ -62,7 +44,7 @@ class ATMapper:
         for audio_path, dur in zip(self.audio_paths, self.durs):
             chunks = self.recognize(audio_path, dur)
             for chunk in chunks:
-                if chunk.text = '':
+                if chunk.text == '':
                     continue
                 chunk_pos = self.chunk_search(chunk.text, pos, min(pos + deep_coef * len(chunk), len(words)))
                 markup.append(namedtuple('Markup', ['tstart', 'tend', 'num'])(chunk.tstart, chunk.tend, chunk_pos))
@@ -72,7 +54,7 @@ class ATMapper:
 
     def make_mapping(self):
         markup = self.map_words()
-        self.word_sec = {i:None for i in range(len(words)}
+        self.word_sec = {i:None for i in range(len(words))}
         self.sec_word = []
         for i in range(len(markup)):
             pos = markup[i].num
@@ -87,3 +69,11 @@ class ATMapper:
                 self.sec_word[-1].update(dict(chink_sec_word))
 
         assert not(None in set(self.word_sec.values()))
+
+
+    def get_word_sec(self):
+        return self.word_sec
+
+
+    def get_sec_word(self):
+        return self.sec_word
