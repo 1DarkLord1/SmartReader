59b5d4bff4c92900e6430c118df99a6b7c8a6e73
alexgrebenshchikov <qazwsx13240987@gmail.com>
2020-05-19 13:39:58 +0500

    reorganized structure of interface

---
+++ test/test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from test_atb_model.test_atb_model import test_atb_model
+from test_audio_text_mapper.test_audio_text_mapper import test_audio_text_mapper
+
+if __name__ == '__main__':
+    unittest.main()
