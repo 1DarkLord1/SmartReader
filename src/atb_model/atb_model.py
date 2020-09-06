608679bf40258061d716f7272757515ecd1357dc
dword <camm2016@yandex.ru>
2020-09-06 19:46:15 +0300

    + logs

--- src/atb_model/atb_model.py
+++ src/atb_model/atb_model.py
@@ -21,12 +21,17 @@ from collections import namedtuple
 from audio_text_mapper import ATMapper
 import shutil
 import threading
+import logging
 
 
 class Model:
-    def __init__(self):
-        self.root_name = 'books'
+    def __init__(self, file=False):
+        if file == True:
+            logging.basicConfig(filename=gen_path('..', '..', 'logs', 'logs.txt'), filemode='w'))
+        else:
+            logging.basicConfig(level=logging.INFO)
 
+        self.root_name = 'books'
         self.tree = None
         self.root = None
         self.fb2_dir = None
@@ -46,6 +51,7 @@ class Model:
 
 
     def get_fb2_root(self, fb2_path):
+        logging.info('get_fb2_root(): fb2_path={}\n'.format(fb2_path))
         fb2_path = gen_path(['..', '..', fb2_path])
         fb2_tree = etree.parse(fb2_path, etree.XMLParser(remove_blank_text=True))
         fb2_root = fb2_tree.getroot()
@@ -55,7 +61,8 @@ class Model:
         return fb2_root
 
 
-    def load_text(self):
+    def load_fb2_text(self):
+        logging.info('load_fb2_text()\n')
         book_path = self.root.find('fb2').text
         fb2_root = self.get_fb2_root(book_path)
         raw_text = etree.tostring(fb2_root.find('body'), encoding='utf-8').decode('utf-8')
@@ -64,53 +71,68 @@ class Model:
 
 
     def make_word_list(self):
+        logging_info('make_word_list()\n')
         self.word_list = [''.join(list(filter(lambda ch: ch.isalpha() or ch.isdigit(), word))).lower()
                          for word in self.text.split()]
         self.word_list = list(filter(lambda word: word != '', self.word_list))
 
 
     def parse_audio_list(self):
+        logging.info('parse_audio_list()\n')
         audio = self.root.find('audio')
         self.audio_list = [gen_path(['..', '..', file.text]) for file in audio.findall('file')]
 
 
     def load(self, path):
+        logging.info('load(): path={}\n'.format(path))
         self.tree = etree.parse(path, etree.XMLParser(remove_blank_text=True))
         self.root = self.tree.getroot()
         self.fb2_name = self.root.find('fb2').text.split('/')[-1].replace('.fb2', '')
         self.fb2_dir = self.root.find('fb2').text.replace(self.fb2_name + '.fb2', '')
-        self.load_text()
+        self.load_fb2_text()
         self.make_word_list()
         self.parse_audio_list()
+        logging.info('Fb2 file {} loaded\n'.format(self.fb2_name))
 
 
     def create_wav_from_mp3(self, audio_path):
+        logging.info('create_wav_from_mp3(): audio_path={}\n'.format(audio_path))
         source = AudioSegment.from_mp3(audio_path)
-        source.export(audio_path.replace('mp3', 'wav'), format='wav')
+        wav_path = audio_path.replace('mp3', 'wav')
+        source.export(wav_path, format='wav')
+        logging.info('Wav file {} created\n'.format(wav_path))
         return int(source.duration_seconds)
 
 
     def make_mapping(self):
+        logging.info('make_mapping()\n')
         self.durs = []
         for audio in self.audio_list:
             self.durs.append(self.create_wav_from_mp3(audio))
+        logging.info('Wav files created\n')
 
         self.wav_list = list(map(lambda audio_path: audio_path.replace('mp3', 'wav'), self.audio_list))
         mapper = ATMapper(atb_mdl=self, last_inv_speed=0.4, div_coef=2.5, pow_coef=1/3)
+        logging.info('ATMapper created\n')
 
         self.word_sec = {}
         self.sec_word = [{} for i in range(len(self.audio_list))]
         self.seconds = [[] for i in range(len(self.audio_list))]
         mapper.map_all()
+        logging.info('All book mapped\n')
 
         for wav_audio in self.wav_list:
             os.remove(wav_audio)
+        logging.info('Wav files removed\n')
+
         mapinfo_path = gen_path(['..', '..', self.fb2_dir, 'mapinfo_' + self.fb2_name + '.dat'])
         with open(mapinfo_path, "wb") as file:
             dill.dump(self.word_sec, file)
+        logging.info('Mapinfo saved\n')
 
 
     def load_map(self):
+        logging.info('load_map()\n')
         mapinfo_rel_path = self.root.find('mapinfo').text
         mapinfo_path = gen_path(['..', '..', mapinfo_rel_path])
         with open(mapinfo_path, "rb") as file:
@@ -127,17 +149,21 @@ class Model:
             self.sec_word[audio_num].update([(sec, word)])
             self.seconds[audio_num].append(sec)
         self.seconds = [sorted(cur_secs) for cur_secs in self.seconds]
+        logging.info('Mapinfo loaded\n')
 
 
     def import_book(self, book_path, audio_paths):
+        logging.info('import_book(): book_path={}, audio_paths={}\n'.format(book_path, audio_paths))
         book_name = book_path.split('/')[-1].replace('.fb2', '')
         folder_path = gen_path(['..', '..', self.root_name, book_name])
         if os.path.exists(folder_path):
             shutil.rmtree(folder_path)
         os.mkdir(folder_path)
         shutil.copyfile(book_path, os.path.join(folder_path, book_name + '.fb2'))
+        logging.info('Books have been copied\n')
         for audio_path in audio_paths:
             shutil.copyfile(audio_path, os.path.join(folder_path, audio_path.split('/')[-1]))
+        logging.info('Audios have been copied\n')
         rel_folder_path = os.path.join(self.root_name, folder_path.split('/')[-1])
         rel_book_path = os.path.join(rel_folder_path, book_name + '.fb2')
         rel_audio_paths = [os.path.join(rel_folder_path, path.split('/')[-1]) for path in audio_paths]
@@ -145,6 +171,8 @@ class Model:
 
 
     def make_atb(self, book_path, book_name, audio_paths, rel_folder_path, folder_path):
+        logging.info('make_atb(): book_path={}, book_name={}, audio_paths={}, rel_folder_path={}, folder_path={}\n'.format(book_path, book_name,
+        audio_paths, rel_folder_path, folder_path))
         atb_root = etree.Element('atb')
         fb2 = etree.SubElement(atb_root, 'fb2')
         fb2.text = book_path
@@ -158,21 +186,26 @@ class Model:
         atb_tree = etree.ElementTree(atb_root)
         atb_tree.write(os.path.join(folder_path, book_name + '_data' + '.atb'), pretty_print=True, xml_declaration=True,
                        encoding='utf-8')
+        logging.info('Atb created\n')
 
 
     def get_audio_list(self):
+        logging.info('get_audio_list()\n')
         return self.audio_list
 
 
     def get_text(self):
+        logging.info('get_text()\n')
         return self.text
 
 
     def get_word_list(self):
+        logging.info('get_word_list()\n')
         return self.word_list
 
     def get_sec(self, word):
         self.mutex.acquire()
+        logging.info('get_sec(): word={}\n'.format(word))
         sec = None
         if self.word_sec:
             sec = self.word_sec.get(word, None)
@@ -182,6 +215,7 @@ class Model:
 
     def get_word(self, audio_num, sec):
         self.mutex.acquire()
+        logging.info('get_word(): audio_num={}, sec={}\n'.format(audio_num, sec))
         if not self.seconds:
             self.mutex.release()
             return None
@@ -198,6 +232,7 @@ class Model:
 
     def get_len_word_sec(self):
         self.mutex.acquire()
+        logging.info('get_len_word_sec()\n')
         res = 0
         if self.word_sec:
             res = round(len(self.word_sec) / len(self.word_list) * 100, 1)
--- src/audio_text_mapper/audio_text_mapper.py
+++ src/audio_text_mapper/audio_text_mapper.py
@@ -20,11 +20,13 @@ class ATMapper:
 
 
     def handle_end(self, audio_num, endp):
+        logging.info('handle_end(): audio_num={}, endp={}'.format(audio_num, endp))
         if self.last_chunk.pos:
             self.write_mapinfo(audio_num, self.map_segment(self.last_chunk.pos, endp, self.last_chunk.tstart, self.mdl.durs[audio_num]))
 
 
     def map_all(self):
+        logging.info('map_all()\n')
         chunk_time = 15
         audio_num = 0
         for dur, audio_path in zip(self.mdl.durs, self.mdl.wav_list):
@@ -33,14 +35,17 @@ class ATMapper:
 
 
     def get_chunk(self, recognized, start_pos, time):
+        logging.info('get_chunk(): recognized={}, start_pos={}, time={}'.format(recognized, start_pos, time))
         chunk_words = [''.join(list(filter(lambda ch: ch.isalpha() or ch.isdigit(), word))).lower()
                         for word in recognized.split()]
         chunk_words = list(filter(lambda word: word != '', chunk_words))
         recognized_chunk = self.Chunk(chunk_words, time, self.chunk_find(chunk_words, start_pos))
+        logging.info('Recognized chunk: {}'.format(recognized_chunk))
         return recognized_chunk
 
 
     def handle_start(self, audio_path, audio_num, chunk_time):
+        logging.info('handle_start(): audio_path={}, audio_num={}, chunk_time={}'.format(audio_path, audio_num, chunk_time))
         rec = sr.Recognizer()
         audio = sr.AudioFile(audio_path)
         start_time = 0
@@ -58,15 +63,18 @@ class ATMapper:
 
 
     def make_mapping(self, audio_path, audio_num, dur, chunk_time):
+        logging.info('make_mapping(): audio_path={}, audio_num={}, dur={}, chunk_time={}\n'.format(audio_path, audio_num, dur, chunk_time))
         start_chunk_pos, self.startp = self.handle_start(audio_path, audio_num, chunk_time)
         self.handle_end(audio_num - 1, start_chunk_pos)
         self.last_chunk = self.Chunk([], chunk_time, start_chunk_pos)
         segm_count = math.ceil(dur ** pow_coef / div_coef)
         fragm_step = math.floor(dur / segm_count)
+        logging.info('fragm_step: {}, fragm_count: {}\n'.format(fragm_step, fragm_count))
         rec = sr.Recognizer()
         audio = sr.AudioFile(audio_path)
         with audio as source:
             for time in range(fragm_step, dur + 1, fragm_step):
+                logging.info('Cur time: {}\n'.format(time))
                 recorded = rec.record(source, offset=fragm_step - chunk_time, duration=chunk_time)
                 recognized = ''
                 try:
@@ -86,16 +94,19 @@ class ATMapper:
 
 
     def average_metrics(self, chunk, text_words):
+        logging.info('average_metrics(): chunk={}, text_words={}\n'.format(chunk, text_words))
         return mean([distance(chunk_word, text_word) for chunk_word, text_word in zip(chunk, text_words)])
 
 
     def chunk_search(self, chunk, startp, endp):
+        logging.info('chunk_search(): chunk={}, startp={}, endp={}\n'.format(chunk, startp, endp))
         endp = endp - len(chunk) + 1
         matches = [(i, self.average_metrics(chunk, self.mdl.word_list[i:i + len(chunk)])) for i in range(startp, endp)]
         return min(matches, key=lambda match: match[1])[0]
 
 
     def map_segment(self, start_pos, end_pos, start_time, end_time):
+        logging.info('map_segment(): start_pos={}, end_pos={}, start_time={}, end_time={}\n'.format(start_pos, end_pos, start_time, end_time))
         dur = end_time - start_time
         syll_cnt = [0]
         symbs = {'а', 'о', 'е', 'и', 'ы', 'у', 'э', 'ё', 'я', 'ю', '.', '?', '!'}
@@ -103,6 +114,7 @@ class ATMapper:
             word = self.mdl.word_list[word_num]
             syll_cnt.append(syll_cnt[-1] + sum(1 if ch[1] in symbs else 0 for ch in enumerate(word)))
         last_inv_speed = (end_pos - start_pos) / dur
+        logging.info('Last inv speed: {}\n'.format(last_inv_speed))
         return [self.Markup(word_num, start_time + (dur / syll_cnt[-1]) * syll_cnt[word_num - start_pos]) for word_num
                 in range(start_pos, end_pos)]
 
@@ -113,6 +125,7 @@ class ATMapper:
 
     def write_mapinfo(self, audio_num, markup):
         self.mdl.mutex.acquire()
+        logging.info('write_mapinfo(): audio_num={}, markup={}\n'.format(audio_num, markup))
         self.mdl.sec_word[audio_num].update([(word.sec, word.word_num) for word in markup])
         self.mdl.word_sec.update([(word.word_num, self.Time(audio_num, word.sec)) for word in markup])
         self.mdl.seconds[audio_num] += [word.sec for word in markup]
